87c0cb897ce26fa30a903987ff226f1f7f3654af
elainees-devs <elainees.devs@gmail.com>
2026-02-27 21:35:38 +0300

    feat(settings): create ProviderAccountSettings component scaffold

---
+++ src/pages/ProviderAccountSettings.tsx
@@ -0,0 +1,10 @@
+const ProviderAccountSettings: React.FC = () => {     
+    return (
+        <div className="provider-account-settings">
+            <h1>Provider Account Settings</h1>  
+            {/* Add provider-specific settings here */}
+        </div>
+    );
+};
+
+export default ProviderAccountSettings;
